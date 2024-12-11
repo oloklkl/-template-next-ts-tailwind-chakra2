554248db6f8fd5d522b52522faa6ce55e5ccd99a
oloklkl <sungju7694@naver.com>
2024-12-11 16:10:26 +0900

    first commit

---
+++ src/components/layout/header/Header.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import Link from 'next/link';
+import React from 'react';
+import { Button, Flex, Spacer } from '@chakra-ui/react';
+import MenuHam from './Menu';
+import { Bell, Search } from 'lucide-react';
+
+const headerButtons = [
+    {
+        label: '알림',
+        icon: Bell,
+        onClick: () => {
+            alarm();
+        },
+    },
+    {
+        label: '검색',
+        icon: Search,
+        onClick: () => {
+            search();
+        },
+    },
+];
+
+const alarm = () => {
+    console.log('알림');
+};
+
+const search = () => {
+    console.log('검색');
+};
+
+const Header = () => {
+    return (
+        <header className='py-2'>
+            <Flex className='container items-center'>
+                <h1>
+                    <Link href={'/'}>logo</Link>
+                </h1>
+                <Spacer />
+                <div>
+                    {headerButtons.map(({ label, icon: Icon, onClick }) => (
+                        <Button size={'sm'} variant='ghost' className='!ps-2 !pe-2' onClick={onClick}>
+                            <Icon size={18} />
+                            <span className='sr-only'>{label}</span>
+                        </Button>
+                    ))}
+                    <MenuHam />
+                </div>
+            </Flex>
+        </header>
+    );
+};
+
+export default Header;
